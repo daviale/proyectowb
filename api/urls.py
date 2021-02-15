8d53fb27e8b29ee4695092a2cd31bcc3d5fa4e52
David <i1920146@continental.edu.pe>
2021-02-15 23:22:52 +0100

    funcviewurl

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api import views
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
-from rest_framework_jwt.views import verify_jwt_token
+
 
 router = routers.DefaultRouter()
 router.register(r'roles', views.RolViewset )
@@ -19,9 +17,7 @@ router.register(r'Clientes', views.ClientViewset )
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path(r'auth/login', obtain_jwt_token),
-    path(r'auth/refresh-token', refresh_jwt_token),
-    path(r'auth/verify-token', verify_jwt_token),
+    
 
     
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
+from api.serializers import RolSerializer
+from api.serializers import DireccionSerializer
+from api.serializers import CategorySerializer
+from api.serializers import ProductSerializer
+from api.serializers import ListSerializer
+from api.serializers import UsuarioSerializer
+from api.serializers import Clientserializer
+
+
+from Tiendaonline.models import Rol
+from Tiendaonline.models import Direccion
+from Tiendaonline.models import Category
+from Tiendaonline.models import Product
+from Tiendaonline.models import List
+from Tiendaonline.models import Usuario
+from Tiendaonline.models import client
 # Create your views here.
+
+
+class RolViewset(viewsets.ModelViewSet):
+    queryset = Rol.objects.all()
+    serializer_class = RolSerializer
+
+class DireccionVIewset(viewsets.ModelViewSet):
+    queryset = Direccion.objects.all()
+    serializer_class = DireccionSerializer   
+
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer 
+
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer  
+
+class ListViewset(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer   
+
+class UsuarioViewset(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer   
+
+class ClientViewset(viewsets.ModelViewSet):
+    queryset = client.objects.all()
+    serializer_class = Clientserializer       
+
